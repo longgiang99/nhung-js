2177d29ae3f9437c5258952862c7079b1b5220b4
trieu <longgiang9999.gl@gmail.com>
2023-02-23 23:27:34 +0700

    diem trung binh va tong

--- toantu.js
+++ toantu.js
@@ -1,6 +1,21 @@
-inputWidth = prompt("Enter the width");
-inputHeight = prompt("Enter the height");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
-let area = width * height;
-document.write("The area is: " + area);
+
+// nhap diem 3 mon toan, van, anh
+let toan = 9
+let van = 8
+let anh = 7
+//cong diem 3 mon
+let tong = toan + van + anh
+let trungbinh = tong / 3
+//hien thi gia tri
+console.log(trungbinh)
+console.log(tong)
+//viet gia tri ra man hinh
+document.write('toan = ' + toan)
+document.write('<br/>')
+document.write('van = ' + van)
+document.write('<br/>')
+document.write('anh = ' + anh)
+document.write('<br/>')
+document.write("tong = " + tong)
+document.write('<br/>')
+document.write('trungbinh = ' + trungbinh)
