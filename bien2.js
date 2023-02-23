ebb7bc56a72c6847ded79f6eb597a8d0c6a4ee9d
trieu <longgiang9999.gl@gmail.com>
2023-02-23 16:50:32 +0700

    chia 2 so

---
+++ bien2.js
@@ -0,0 +1,10 @@
+
+var a,b;
+//lay gia tri tu nguoi dung
+a = prompt("nhap so a");
+b = prompt("nhap so b");
+//doi sang kieu so
+a = parseInt(a);
+b = parseInt(b);
+// in ket qua ra man hinh
+document.write("Phần dư khi chia hai số " + a + " và " + b + " là " + (a % b) + "<br/>");
