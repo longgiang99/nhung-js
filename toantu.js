75b28f1c4cc9b79a5cfc1a9924c3fbe44fd36563
trieu <longgiang9999.gl@gmail.com>
2023-02-24 10:21:43 +0700

    chuen doi nhit do

--- toantu.js
+++ toantu.js
@@ -1,21 +1,4 @@
-
-// nhap diem 3 mon toan, van, anh
-let toan = 9
-let van = 8
-let anh = 7
-//cong diem 3 mon
-let tong = toan + van + anh
-let trungbinh = tong / 3
-//hien thi gia tri
-console.log(trungbinh)
-console.log(tong)
-//viet gia tri ra man hinh
-document.write('toan = ' + toan)
-document.write('<br/>')
-document.write('van = ' + van)
-document.write('<br/>')
-document.write('anh = ' + anh)
-document.write('<br/>')
-document.write("tong = " + tong)
-document.write('<br/>')
-document.write('trungbinh = ' + trungbinh)
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 9;
+}
